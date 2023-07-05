e4748aad3343c65d6f4b8de765c1222dcc825ed7
Raihanul Islam Refat <refat0167@gmail.com>
2023-07-05 20:24:54 +0600

    feat: versioning

--- cmd/decode_zatca_qr/main.go
+++ cmd/decode_zatca_qr/main.go
@@ -1,7 +1,29 @@
 package main
 
-import "github.com/raihanul-2k15/zatca-qr-reader/cli"
+import (
+	"fmt"
+	"os"
+
+	"github.com/raihanul-2k15/zatca-qr-reader/cli"
+)
+
+var VERSION string = "current" // to be replaced by -ldflags="-X main.VERSION=xyz"
 
 func main() {
+	versionArgs := []string{"-v", "--version", "version"}
+	if len(os.Args) > 1 && contains(versionArgs, os.Args[1]) {
+		fmt.Println(VERSION)
+		os.Exit(0)
+	}
+
 	cli.Run()
+}
+
+func contains(args []string, arg string) bool {
+	for _, a := range args {
+		if a == arg {
+			return true
+		}
+	}
+	return false
 }
